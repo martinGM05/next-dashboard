bc558ea5b234defa7e2910d0771a973074232cd3
Martin Gonzalez <mtn.gm05@gmail.com>
2024-06-09 12:15:08 -0600

    ✨ Feat: Introduce core dashboard

---
+++ src/app/dashboard/counter/page.tsx
@@ -0,0 +1,19 @@
+import { CartCounter } from "../shopping-cart";
+
+
+export const metadata = {
+  title: "Shopping Cart",
+  description: "A simple counter page",
+  keywords: "counter, page",
+}
+
+export default function CounterPage() {
+
+
+  return (
+    <div className="flex flex-col items-center justify-center w-full h-full">
+      <span>Productos en el carrito de compras</span>
+      <CartCounter value={20} />
+    </div>
+  );
+}
